0598b5ca5dec4c48d7e73042c0baf97b8ef3f5d1
Xopper <ali.haloua@usmba.ac.ma>
2021-03-03 18:54:32 +0100

    add some styling to profile[not completed :)

--- client/src/Pages/profile.jsx
+++ client/src/Pages/profile.jsx
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import matcha from '../img/matcha2.jpg';
 import { useParams } from 'react-router-dom';
 import ProtectedRoute from '../routes/protectedRoute';
 
 function Profile() {
+	const [data, setData] = useState({
+		img:
+			'https://www.hindishayaricollections.com/wp-content/uploads/2020/03/beautifull-girls-images-download-46.jpg',
+		name: ''
+	});
 	const { username } = useParams();
 	return (
-		<div className='auth__container'>
-			<div className='auth'>
-				<section className='auth__first_section'>
-					<div className='auth__first'>
-						<div>tach {`${username}`}</div>
-					</div>
-				</section>
-				<section
-					className='auth__second_section'
-					style={{
-						backgroundImage: `url(${matcha})`
-					}}
-				>
-					<div className='auth__second'>
-						<div className='auth__second__text'>
-							<h3>Shop With Confidendce</h3>
-							<p>
-								Browse a catalog of ecommerce services by our vetted experts or submit custom requests.
-							</p>
+		<div className='profile__container'>
+			<div className='profile__wrapper'>
+				<section className='profile__content'>
+					<section className='profile__heeder'>
+						<div className='img__wrapper'>
+							<img src={data.img} alt='tacos' />
+							<span className='profile__status'></span>
 						</div>
-					</div>
+						<div className='profile__fullname'>tach {`${username}`}</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>
+								<p>Contry.</p>
+								<span></span>
+							</div>
+							<div className='profile__fieldset--value'>Brazil</div>
+						</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>
+								<p>Gender.</p>
+								<span></span>
+							</div>
+							<div className='profile__fieldset--value'>Male</div>
+						</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>
+								<p>Interestes.</p>
+								<span></span>
+							</div>
+							<div className='profile__fieldset--value'>Male</div>
+						</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>
+								<p>Tags.</p>
+								<span></span>
+							</div>
+							<div className='profile__fieldset--value'>Male, tach, lobe, president</div>
+						</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>Gender.</div>
+							<div className='profile__fieldset--value'>Male</div>
+						</div>
+						<div className='profile__fieldset'>
+							<div className='profile__fieldset--key'>Biography.</div>
+							<div className='profile__fieldset--value'>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores quae voluptate
+								magni consequuntur itaque est voluptas hic rerum unde neque ad rem modi, saepe nulla,
+								non repudiandae ipsam odio ipsum!
+							</div>
+						</div>
+					</section>
 				</section>
 			</div>
 		</div>
